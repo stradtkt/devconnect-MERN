20e1e18bbcdfdc21adb470e3824c65fd59b93d95
webkit-development <webkit.development@gmail.com>
2022-12-15 09:31:52 -0500

    Added some of the client

---
+++ client/src/components/auth/Login.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    const [formData, setFormData] = useState({
+        email: '',
+        password: ''
+    });
+    const {email, password} = formData;
+
+    const onChange = (e) => {
+        setFormData({...formData, [e.target.name]: e.target.value});
+    }
+    const onSubmit = async (e) => {
+        e.preventDefault();
+        console.log('Successfully Logged In');
+    }
+  return (
+    <>
+    <h1 className="large text-primary">Sign In</h1>
+      <p className="lead"><i className="fas fa-user"></i> Login To Your Account</p>
+      <form className="form" onSubmit={e => onSubmit(e)}>
+        <div className="form-group">
+          <input type="email" placeholder="Email Address" name="email" onChange={e => onChange(e)} value={email}/>
+        </div>
+        <div className="form-group">
+          <input
+            type="password"
+            placeholder="Password"
+            name="password"
+            minLength="6"
+            onChange={e => onChange(e)}
+            value={password}
+          />
+        </div>
+        <input type="submit" className="btn btn-primary" value="Login" />
+      </form>
+      <p className="my-1">
+        Need an account? <Link to="/register">Sign Up</Link>
+      </p>
+    </>
+  );
+}
+
+export default Login;
